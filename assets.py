d603fe2d66218b481f6180a6d015100c85f0af60
code-monk08 <mayank.singh081997@gmail.com>
2019-09-21 03:27:09 +0530

    formatted code using black

--- assets.py
+++ assets.py
@@ -2,5 +2,5 @@ import os
 
 from pygame.image import load
 
-crossimg = load(os.path.join("images","CROSS.png"))
-noughtimg = load(os.path.join("images","NOUGHT.png"))
+crossimg = load(os.path.join("images", "CROSS.png"))
+noughtimg = load(os.path.join("images", "NOUGHT.png"))
